bd38a0c5966b7396edc37d2283803cdf7a41936a
James Percival <j.percival@imperial.ac.uk>
2016-04-26 15:56:44 +0100

    fluidity build.

--- fluidity.rb
+++ fluidity.rb
@@ -1,12 +1,11 @@
 
 class Fluidity < Formula
   desc ""
-  homepage "FluidityProject.github.io"
-  url "https://github.com/FluidityProject/fluidity.git", :branch => "ElCapitanFixes"
+  homepage "http://FluidityProject.github.io"
+  url "https://github.com/FluidityProject/fluidity.git", :branch => "system_judy_spatialindex"
   sha256 ""
-  version "4.elcapitan"
+  version "4.13"
 
-  depends_on "gcc"
   depends_on :mpi => [:cc, :cxx, :f77, :f90]
   depends_on :fortran
   depends_on :x11
@@ -16,7 +15,10 @@ class Fluidity < Formula
   depends_on "numpy"
   depends_on "udunits"
   depends_on "gnu-sed"
-  depends_on "vtk5"
+  depends_on "origimbo/tools/vtk5"
+  depends_on "judy"
+  depends_on "spatialindex"
+  depends_on "origimbo/tools/libspud"
 
   option "enable-2d-adaptivity", "Build and link libmba2 for 2d adaptivity"
   option "enable-debug", "Build debug version"
@@ -24,12 +26,7 @@ class Fluidity < Formula
   keg_only "Lets test this first!"
 
   fails_with :llvm 
-  fails_with :clang
   fails_with :gcc_4_0
-  fails_with :gcc do
-    build 5666
-    cause "need a recent version of gcc."
-  end
 
   def oprefix(f)
     Formula[f].opt_prefix
@@ -38,17 +35,15 @@ class Fluidity < Formula
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
 
-    ENV["CC"] = "#{HOMEBREW_PREFIX}/bin/mpicc"
-    ENV["CXX"] = "#{HOMEBREW_PREFIX}/bin/mpicxx"
-    ENV["F77"] = "mpif90"
-    ENV["FC"] = "mpif90"
-
     ENV["CPPFLAGS"] = "-I#{oprefix("origimbo/tools/zoltan")}/include -I#{oprefix("vtk5")}/include/vtk-5.10"
     ENV["LDFLAGS"] = "-L#{oprefix("vtk5")}/lib/vtk-5.10 -lvtkIO -lvtkHybrid -lvtkGraphics -lvtkRendering -lvtkFiltering -lvtkCommon"
     
     ENV["PETSC_DIR"] = "#{HOMEBREW_PREFIX}/opt/petsc-fluidity"
 
-    system "./configure", "--prefix=#{prefix}"
+    system "./configure", "--prefix=#{prefix}",
+                          "--with-libspud-root=#{HOMEBREW_PREFIX}",
+                          "--with-spatialindex-root=#{HOMEBREW_PREFIX}",
+                          "--with-judy=#{oprefix("judy")}/lib/libjudy.a"
     system "make"
     system "make", "install"
   end
